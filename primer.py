26977a7fbcf89df31807bb187e90ad35a3ace711
AlkimistaRavith <ravith.d3@gmail.com>
2025-05-08 23:46:21 +0000

    inicio

---
+++ primer.py
@@ -0,0 +1,19 @@
+# Day 2: 30 Days of python programming
+
+name = "david"
+apellido = "medina"
+full = name + apellido
+country = "Chile"
+city = "Hualpen"
+age = 35
+year = 1989
+
+print(name)
+print(apellido)
+print(full)
+print(age)
+
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+
